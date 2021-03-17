6085f2ed4239151dec2fca11c6b4987ba628aee1
Homam <ishomam@gmail.com>
2021-03-17 14:40:29 +0100

    New functions in mpl_utils

--- bmcs_utils/api.py
+++ bmcs_utils/api.py
@@ -3,7 +3,7 @@ from bmcs_utils.interactive_window import InteractiveWindow
 from bmcs_utils.interactive_model import InteractiveModel
 from bmcs_utils.view import View
 from bmcs_utils.item import Item
-from bmcs_utils.mpl_utils import mpl_align_xaxis, mpl_align_yaxis
+from bmcs_utils.mpl_utils import mpl_align_xaxis, mpl_align_yaxis, mpl_align_yaxis_to_zero, mpl_show_one_legend_for_twin_axes
 from bmcs_utils.symb_expr import SymbExpr, InjectSymbExpr
 from bmcs_utils.trait_types import Int, Float, Bool, Button, Range, Progress, Array
 from bmcs_utils.editors import \
