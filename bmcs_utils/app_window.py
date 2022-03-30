200848a8ef06834b8df12fd782d4c04545388918
rch <rostislav.chudoba@rwth-aachen.de>
2022-03-30 21:30:48 +0200

    Version working with VisualStudio

--- bmcs_utils/app_window.py
+++ bmcs_utils/app_window.py
@@ -61,10 +61,9 @@ class K3DBackend(PlotBackend):
         self.plot_widget = ipw.Output(layout=ipw.Layout(width="100%", height="100%"))
         self.plot_fig = k3d.Plot()
         self.plot_fig.layout = ipw.Layout(width="100%",height="100%")
-        with self.plot_widget:
-            self.plot_fig.display()
+        # with self.plot_widget:
+        #     self.plot_fig.display()
         self.plot_fig.outputs.append(self.plot_widget)
-
         self.objects = {}
 
     def clear_fig(self):
@@ -86,7 +85,8 @@ class K3DBackend(PlotBackend):
         self.objects.pop(object_key)
 
     def show_fig(self):
-        pass
+        with self.plot_widget:
+            self.plot_fig.display()
     def setup_plot(self, model):
         model.setup_plot(self)
     def update_plot(self, model):
