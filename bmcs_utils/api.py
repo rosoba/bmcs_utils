2b20ac9bc0a09d42c783571b88ee04ee80520f0a
ishomam <ishomam@gmail.com>
2022-07-21 11:25:52 +0200

    Some plot tools are added

--- bmcs_utils/api.py
+++ bmcs_utils/api.py
@@ -12,6 +12,7 @@ from bmcs_utils.editors import \
     IntEditor, BoolEditor, FloatEditor, FloatRangeEditor, ProgressEditor, \
     ButtonEditor, ArrayEditor, InstanceEditor, EitherTypeEditor, ListEditor, \
     HistoryEditor, TextAreaEditor, IntRangeEditor, FloatSliderEditorSelector, FloatSliderEditor
+from bmcs_utils.misc.plot_tools import plot_colors, set_latex_mpl_format
 from bmcs_utils.parametric_study import ParametricStudy
 from bmcs_utils.data_cache import data_cache
 from bmcs_utils.k3d_utils.extrusion_for_3d_curve import Extruder
