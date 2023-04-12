453e16276deffbe85faeb6f817b3546d2060296b
ishomam <ishomam@gmail.com>
2023-04-12 09:40:59 +0200

    Add font_size attr to set_latex_mpl_format()

--- bmcs_utils/misc/plot_tools.py
+++ bmcs_utils/misc/plot_tools.py
@@ -3,9 +3,9 @@ import matplotlib.pyplot as plt
 plot_colors = ['#000000', '#bc2122', '#55679e', '#69b628', '#dacf2f', '#ff6600']
 
 
-def set_latex_mpl_format():
+def set_latex_mpl_format(font_size = 15):
     plt.rcParams["font.family"] = "Times New Roman"
-    plt.rcParams["font.size"] = 15
+    plt.rcParams["font.size"] = font_size
     # To have math like LaTeX
     plt.rcParams['mathtext.fontset'] = 'cm'
     plt.rcParams['mathtext.rm'] = 'serif'
